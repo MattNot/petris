7970d19d51e8a06b810cd57dcf2765e740754028
MatteoLoria <mloria58@gmail.com>
2019-04-09 19:34:12 +0200

    stavolta

--- desktop/src/com/petris/desktop/DesktopLauncher.java
+++ desktop/src/com/petris/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.tetris.desktop;
+package com.petris.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.tetris.Tetris;
+import com.petris.Petris;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Tetris(), config);
+		new LwjglApplication(new Petris(), config);
 	}
 }
