5042b67a80eca51b0e93836d06a5026fb5f21f3b
MattNot <matteoatnotaro@outlook.it>
2019-04-14 12:52:47 +0200

    added positioning

--- core/src/com/petris/Petris.java
+++ core/src/com/petris/Petris.java
@@ -1,5 +1,7 @@
 package com.petris;
 
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -11,6 +13,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
+import com.petris.pieces.Block;
 import com.petris.pieces.Piece;
 import com.petris.pieces.PieceI;
 import com.petris.pieces.PieceLRight;
@@ -24,46 +27,106 @@ public class Petris extends ApplicationAdapter {
 	SpriteBatch batch;
 	ShapeRenderer sh;
 	OrthographicCamera camera;
+	Rectangle[] borders;
 	Texture img;
-	Piece l;
+	Piece actual;
+	Block map[][];
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera(800,600);
 		sh = new ShapeRenderer();
-		l = new PieceLLeft(Color.BLUE);
+		actual = new PieceI(Color.BLUE);
 		camera.setToOrtho(true, 800, 600);
 		sh.setProjectionMatrix(camera.combined);
+		borders = new Rectangle[3];
+
+		//Ci serve un playground di 400*200 i 90 e 10 sono per fare vedere meglio i bordi
+		borders[0] = new Rectangle(200+90, 0, 10, 400);
+		borders[1] = new Rectangle(400+100, 0, 10, 410);
+		borders[2] = new Rectangle(200+90, 400, 210,10);
+		map = new Block[20][10];
 	}
 
 	@Override
-	public void render () {
+	public void render () { // TODO ASSOLUTAMENTE REFACTOR HAHA
+		boolean canMove = true;
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
 		/*
 		 * L'idea è avere un Piece attuale che viene generato in modo casuale tra i 4 e gestire tutto attraverso
 		 * il polimorfismo dei metodi. Una volta posato "buttarlo via" mettendo i suoi Rectangle dentro un 
 		 * ArrayList di coppie <Rectangle, Color>.
 		 */
-		
-		if(l.getBlocks()[3].getY()<600-Piece.BLOCK_HEIGHT) //TODO facciamo un metodo astratto per sapere la Y in basso?
-			l.move(Gdx.graphics.getDeltaTime()*3);
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && l.getBlocks()[3].getY()<600-Piece.BLOCK_HEIGHT)
-			l.move(Gdx.graphics.getDeltaTime()*10);
-		
-		if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
-			l.moveLeft(Gdx.graphics.getDeltaTime());
-		if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
-			l.moveRight(Gdx.graphics.getDeltaTime());
-		if(Gdx.input.isKeyJustPressed(Input.Keys.UP))
-			l.rotate(); //E' così nel gioco originale
+		for(Rectangle i : actual.getBlocks())
+		{
+			if(i.overlaps(borders[2])) {
+				canMove = false;
+				break;
+			}
+			for(int j = 0; j<map.length; j++) {
+				for(int k=0; k<map[j].length; k++) {
+					if(map[j][k] != null) {
+						if(i.overlaps(map[j][k]))
+						{
+							canMove = false; 
+							break;
+						}
+					}
+				}
+			}
+		}
+		if(canMove) {
+			actual.move(Gdx.graphics.getDeltaTime()*3);
+			if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+				actual.move(Gdx.graphics.getDeltaTime()*3);
+			if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+				actual.moveLeft(Gdx.graphics.getDeltaTime());
+			if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
+				actual.moveRight(Gdx.graphics.getDeltaTime());
+			if(Gdx.input.isKeyJustPressed(Input.Keys.UP))
+				actual.rotate(); //E' così nel gioco originale
+		}else {
+			for(Rectangle i : actual.getBlocks())
+			{
+				map[(int)(i.getY())/20][(int)(i.getX()-300)/20] = new Block(actual.getC(),i);
+			}
+			Random r = new Random();
+			int tmp = r.nextInt(7);
+			switch(tmp) {
+			case 0:
+				actual = new PieceI(Color.BLUE);
+				break;
+			case 1:
+				actual = new PieceLLeft(Color.FOREST);
+				break;
+			case 2:
+				actual = new PieceLRight(Color.CHARTREUSE);
+				break;
+			case 3:
+				actual = new PieceS(Color.CYAN);
+				break;
+			case 4:
+				actual = new PieceT(Color.PINK);
+				break;
+			case 5:
+				actual = new PieceZLeft(Color.ORANGE);
+				break;
+			case 6:
+				actual = new PieceZRight(Color.BROWN);
+				break;
+			}
+		}
 		//Pezzo attuale
 		sh.begin(ShapeType.Filled);
+		sh.setColor(Color.BLACK);
+		sh.rect(borders[0].x, borders[0].y, borders[0].width, borders[0].height);
+		sh.rect(borders[1].x, borders[1].y, borders[1].width, borders[1].height);
+		sh.rect(borders[2].x, borders[2].y, borders[2].width, borders[2].height);
 		int i = 0;
-		for(Rectangle r : l.getBlocks()) { 
+		for(Rectangle r : actual.getBlocks()) { 
 			if(i!=0)
-				sh.setColor(l.getC());
+				sh.setColor(actual.getC());
 			else
 				sh.setColor(Color.BLACK); //DEBUG TO LET US KNOW WHO IS THE FIRST BLOCK
 			i++;
@@ -71,9 +134,18 @@ public class Petris extends ApplicationAdapter {
 		}
 		sh.end();
 		
-		//ArrayList dei pezzi posati
 		sh.begin(ShapeType.Filled);
-		//TODO Disegnare i pezzi posati
+		for(int j=0; j<map.length; j++) {
+			for(int k=0; k<map[j].length; k++) {
+				if(map[j][k]!=null) {
+					sh.setColor(map[j][k].getC());
+					sh.rect(map[j][k].x, map[j][k].y, map[j][k].width, map[j][k].height);
+				}
+				sh.setColor(Color.BLACK);
+				sh.rect(300,0+20*j,200,1f);
+				sh.rect(300+20*k,0,1f,400);
+			}
+		}
 		sh.end();
 	}
 	
---
+++ core/src/com/petris/pieces/Block.java
@@ -0,0 +1,20 @@
+package com.petris.pieces;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Block extends Rectangle{
+	private static final long serialVersionUID = 273836992715437376L;
+	Color c;
+	public Color getC() {
+		return c;
+	}
+	public void setC(Color c) {
+		this.c = c;
+	}
+	public Block(Color c, Rectangle r) {
+		super(r);
+		this.c = c;
+	}
+
+}
